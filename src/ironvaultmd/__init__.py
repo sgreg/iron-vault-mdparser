ca73305a38803745cfcd37fa97e7514b149fff4e
Sven Gregori <sven@craplab.fi>
2025-05-22 01:40:37 +0300

    Add stuff to build a proper package from this

--- src/ironvaultmd/__init__.py
+++ src/ironvaultmd/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.1"
+
 from .ironvault import IronVaultExtension as IronVaultExtension
 from .processors.frontmatter import FrontmatterException as FrontmatterException
 from .processors.mechanics import MechanicsBlockException as MechanicsBlockException
