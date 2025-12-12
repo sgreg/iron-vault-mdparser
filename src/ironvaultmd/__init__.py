9c005bfb5d1e294fedc27fe358069ddb8ac2dcc7
Sven Gregori <sven@craplab.fi>
2025-12-13 00:54:41 +0200

    Bump version to 0.3.0

--- src/ironvaultmd/__init__.py
+++ src/ironvaultmd/__init__.py
@@ -44,7 +44,7 @@ Attributes:
     logger_name: Name of the package logger used throughout the codebase.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 logger_name = "ironvaultmd"
 
 from .ironvault import IronVaultExtension as IronVaultExtension
